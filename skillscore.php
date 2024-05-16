1954cfdf55651bbc9386f7140324eae1fce7043d
davistheweb <smartkelvin775@gmail.com>
2024-05-16 13:07:40 +0100

    new chnages

--- skillscore.php
+++ skillscore.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>IMSU-E || Login - Forms</title>
+    <title>IMSU-E || Confirm Skill Score</title>
     <link rel="stylesheet" href="bootstrap-5.0.2-dist\css\bootstrap.min.css">
     <link href="assets/images/images.png" rel="icon">
     <link rel="stylesheet" href="bootstrap-5.0.2-dist\css\bootstrap-utilities.css">
